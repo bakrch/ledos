a8edcaf68aba8041b7a3fd714f70ac5abdd3235f
CHAKKOR Bakr <bakr.chakk@gmail.com>
2024-04-11 12:39:57 +0200

    Remove prints

--- ledos/gpio/main.go
+++ ledos/gpio/main.go
@@ -48,14 +48,13 @@ func InputController(direction <-chan int) {
 	var (
 		clk rpio.State
 		dt  rpio.State
-		sw  rpio.State
+		//sw  rpio.State
 	)
 	var counter = 0
 	for {
 		clk = clkPin.Read()
 		dt = dtPin.Read()
-		sw = swPin.Read()
-		fmt.Println("dt, clk, sw", dt, clk, sw)
+		//sw = swPin.Read()
 		if clk != baseClk {
 			dt = dtPin.Read()
 			if dtPin.Read() != clk {
@@ -67,13 +66,12 @@ func InputController(direction <-chan int) {
 			if menuChoice < 0 {
 				menuChoice = -1 * menuChoice
 			}
-			fmt.Println("dt ", dt, "clk ", clk, "previousClk", baseClk)
-
+			//fmt.Println("dt ", dt, "clk ", clk, "previousClk", baseClk)
+			oc.CurrentApp().SelectNextComponent()
+			fmt.Println("Current component: ", oc.CurrentApp().ActiveComponent)
 			// To prevent it from reading more than once per rotary encoder rotatio
 			for clkPin.Read() != baseClk {
 			}
-			oc.CurrentApp().SelectNextComponent()
-			fmt.Println("Current component: ", oc.CurrentApp().ActiveComponent)
 		}
 	}
 }
